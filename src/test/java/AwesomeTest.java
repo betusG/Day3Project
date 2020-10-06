34f478772924dc437ce77bfeea4dc83f2a3fa076
betusG <66532814+betusG@users.noreply.github.com>
2020-10-06 21:45:22 +0100

    awesome hello

--- src/test/java/AwesomeTest.java
+++ src/test/java/AwesomeTest.java
@@ -13,5 +13,17 @@ public class AwesomeTest {
         //and now my project is in sync with remote repository
 
         //now i made more awex=some changes and i am about to push this back github
+
+
+        //TASK FINALL
+        /* 1.   go to git hub work my repo into your account
+        *  2  (new) clone the forked repo from your accountinto intelilij
+        *  3  add a folder under  src/test/java with your github username
+        * 4. add a class i=under this username called Hello
+        * 5. push it back to your own github
+        * 6. (new) request the owner of original repo to add your change to original repo
+        *   by opening pill request to merge your change your change into originalk repo
+        *
+        * 7  observe how it might look like in owners  repo */
     }
 }
---
+++ src/test/java/eu3user.java
@@ -0,0 +1,8 @@
+public class eu3user {
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
