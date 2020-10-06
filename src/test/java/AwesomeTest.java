4fcb9890308bd7eae1411264f7345082808a5b45
betusG <66532814+betusG@users.noreply.github.com>
2020-10-06 20:46:16 +0100

    Added new file for awesome feature

---
+++ src/test/java/AwesomeTest.java
@@ -0,0 +1,12 @@
+public class AwesomeTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("Awesome Test");
+
+        //red means it is untrack
+        //green means it s under stating area
+        //grey in idea folder menas it is ignored
+        //when the colour is normal meana commited no changes
+    }
+}
