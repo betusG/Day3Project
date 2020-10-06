6fa960b27964a0dad17f4e4f1c2c36238d818b66
betusG <66532814+betusG@users.noreply.github.com>
2020-10-06 20:59:15 +0100

    ADDED MORE AWESOME STUFF

--- src/test/java/AwesomeTest.java
+++ src/test/java/AwesomeTest.java
@@ -8,5 +8,10 @@ public class AwesomeTest {
         //green means it s under stating area
         //grey in idea folder menas it is ignored
         //when the colour is normal meana commited no changes
+
+        //i  just pulled the new change from github that i didi not lave locally
+        //and now my project is in sync with remote repository
+
+        //now i made more awex=some changes and i am about to push this back github
     }
 }
